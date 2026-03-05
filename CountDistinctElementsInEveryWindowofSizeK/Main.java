0fb9154f849b357d27e99995d8aabfd9570ccf7c
subbaiyamanokaran-svg <subbaiya.manokaran@gmail.com>
2026-03-05 22:31:02 +0530

    distinct count

--- CountDistinctElementsInEveryWindowofSizeK/Main.java
+++ CountDistinctElementsInEveryWindowofSizeK/Main.java
@@ -14,25 +14,29 @@ public class Main {
         System.out.print("Enter window size (k): ");
         int k = sc.nextInt();
 
-        for (int i = 0; i <= arr.size() - k; i++) {
-            int distinctCount = 0;
+        printDistinctInWindow(arr, k);
+    }
 
-            for (int j = i; j < i + k; j++) {
-                boolean isFirstAppearance = true;
-                
-                for (int m = i; m < j; m++) {
-                    if (arr.get(j).equals(arr.get(m))) {
-                        isFirstAppearance = false;
-                        break;
-                    }
-                }
-                
-               
-                if (isFirstAppearance) {
-                    distinctCount++;
-                }
+    public static void printDistinctInWindow(ArrayList<Integer> arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < k; i++) {
+            map.put(arr.get(i), map.getOrDefault(arr.get(i), 0) + 1);
+        }
+        System.out.print(map.size() + " ");
+
+        for (int i = k; i < arr.size(); i++) {
+            int prevElement = arr.get(i - k);
+            if (map.get(prevElement) == 1) {
+                map.remove(prevElement);
+            } else {
+                map.put(prevElement, map.get(prevElement) - 1);
             }
-            System.out.print(distinctCount + " ");
+
+            int newElement = arr.get(i);
+            map.put(newElement, map.getOrDefault(newElement, 0) + 1);
+
+            System.out.print(map.size() + " ");
         }
     }
 }
